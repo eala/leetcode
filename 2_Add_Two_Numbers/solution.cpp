939fa5e0f0fd25bc81936f57b6361b70a9cf1745
hank.wang <eala0113@gmail.com>
2018-02-22 01:34:48 +0800

    fix with working submission

--- 2_Add_Two_Numbers/solution.cpp
+++ 2_Add_Two_Numbers/solution.cpp
@@ -1,52 +1,41 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+* Definition for singly-linked list.
+* struct ListNode {
+*    int val;
+*    ListNode *next;
+*    ListNode(int x) : val(x), next(NULL) {}
+* };
+*/
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode *result = NULL;
-        ListNode *last = NULL;
-        
+        ListNode *tail = NULL;
         int carry = 0;
-        int value = 0;
+        int digitSum = 0;
         
-        while ((l1 || l2) || ((!l1 && !l2) && carry)){
-            ListNode *node = new ListNode(0);
-            
-            if(l1 || l2){
-                if (l1)
-                    value = l1->val;
-                if (l2)
-                    value += l2->val;
-                value = value % 10;
-                node->val = value + carry;
-                
-                if (l1)
-                    carry = l1->val;
-                if (l2)
-                    carry += l2->val;
-                carry = carry / 10;
-
+        while (NULL != l1 || NULL != l2 || 1  == carry){
+            digitSum = carry;
+            if (NULL != l1){
+                digitSum += l1->val;
                 l1 = l1->next;
-                l2 = l2->next;
-            }else{
-                node->val = carry;
-                carry = 0;
             }
+            if (NULL != l2){
+                digitSum += l2->val;
+                l2 = l2->next;
+            }
+
+            int val = digitSum % 10;
+            ListNode *node = new ListNode(val);
+            carry = digitSum / 10;
 
             if (NULL == result){
                 result = node;
-                continue;
             }else{
-                last = result;
-                while (NULL != last->next)
-                    last = last->next;
-                last->next = node;
+                tail = result;
+                while (NULL != tail-> next)
+                    tail = tail-> next;
+                tail-> next = node;
             }
         }
         return result;
