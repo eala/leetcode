5f60e9df4ee23c5dd79c64b79d7e9419c23ac7bf
hank.wang <eala0113@gmail.com>
2018-03-16 00:24:19 +0800

    solve misunderstanding of zigzag order
    for example, 3 rows with five letters
    with (y, x) coordinate, the order is (0, 0), (1, 0), (2, 0), (1, 1), (0, 2)
    
    misunderstanding order: (0, 0), (0, 1), (1, 0), (2, 0), (1, 1), ...

--- 6_Zigzag_Conversion/main.cpp
+++ 6_Zigzag_Conversion/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 #define DEBUG 1
 #define debug_print(fmt, ...) \
@@ -8,89 +9,79 @@
 using namespace std;
 
 /*
-    1. zigzag is scanned diagonally
-    2. each diagonal represents the same distance (sum of x_index & y_index) to left upper corner
-    3. e.g. A B F G
-            C E H
-            D I
-            J
-        distance 0 : A (0, 0)
-                 1 : B (1, 0), C (0, 1)
-                 2 : D (0, 2), E (1, 1), F (2, 0)
-    4. Namely, given sum of distance r, if r is even, x_index increases from 0 to r, y_index decreases from r to 0
-    5. one more restrict is (odd, even) will be filled empty
+    1. zigzag is scanned vertically and then diagonally
+    2. e.g. A     G     M
+            B   F H   L N
+            C E   I K   O
+            D     J     P
+            ^^^^^^
+            1 set occupies (assume n=rows)  n-2+1 columns and contains n+n-2=2n-2 numbers
  */
 
 class Solution {
 public:
+
     string convert(string s, int numRows) {
         string result;
-        // roughly allocate with long size, *2 for worst case numRow = 1
-        int numCols = s.length() * 2;
-        if (0 == numCols) return result;
-        
-        // allocate resource
-        char **data = (char **) malloc(sizeof(char*) * numCols);
-        memset(data, 0, sizeof(char*) * numCols);
-        for (size_t i = 0; i < numCols; i++){
-            data[i] = (char *) malloc(sizeof(char) * numRows);
-            memset(data[i], 0, sizeof(char) * numRows);
+
+        if (numRows <= 1) return s;
+        const int verticalLineCount = 1;
+        const int largestIndexInVerticalLine = numRows - 1;
+        const int setNumbers = 2 * numRows - 2;
+        const int setCols = numRows - 2 + 1;
+
+        // 1. calculate needed cols
+        int neededSetCols = s.length() / setNumbers * setCols;
+        int residueCols = (0 == s.length() % setNumbers)? 0 : verticalLineCount;
+        if (s.length() % setNumbers > largestIndexInVerticalLine)
+            residueCols += s.length() - 1 - largestIndexInVerticalLine;
+        int cols = neededSetCols + residueCols;
+        debug_print("needed cols: %d\n", cols);
+
+        // 2. allocate resource
+        char **rowString = (char **)malloc(sizeof(char *)*numRows);
+        for (size_t i=0; i< numRows; i++){
+            rowString[i] = (char *)malloc(sizeof(char)*cols);
+            memset(rowString[i], 0, sizeof(char)*cols);
         }
 
-        // fill in
-        int distance = 0;
-        int x_index = 0;
-        int y_index = 0;
-
-        debug_print("numRows: %d, numCols: %d\n", numRows, numCols);
-        for (size_t i = 0; i < s.length(); i++){
-            if (1 == x_index % 2 && 0 == y_index % 2){
-                i--;
+        // 3. fill data
+        for (size_t i=0; i< s.length(); i++){
+            int setCol = i / setNumbers * setCols;
+            int residueCol = i - (i / setNumbers * setNumbers);
+            if(residueCol < largestIndexInVerticalLine){
+                residueCol = 0;
             }else{
-                data[x_index][y_index] = s.at(i);
+                residueCol = residueCol - largestIndexInVerticalLine;
+            }
+            int col = setCol + residueCol;
+
+            int row = i % setNumbers;
+            if (row > largestIndexInVerticalLine){
+                row = largestIndexInVerticalLine - residueCol;
             }
 
-            debug_print("data[%d][%d]:%c\n", x_index, y_index, data[x_index][y_index]);
-            if (0 == distance % 2){
-                x_index++;
-                y_index--;
-                if (y_index < 0){
-                    y_index++;
-                }
-            }else{
-                x_index--;
-                y_index++;
-                if (x_index < 0){
-                    x_index++;
-                } 
-                if (y_index > numRows - 1){
-                    x_index += 2;
-                    y_index--;
-                }
-            }
-            distance = x_index + y_index;
+            debug_print("setCol: %d, residueCol: %d\n", setCol, residueCol);
+            debug_print("i: %lu, row:%d, col: %d\n", i, row, col);
+            rowString[row][col] = s[i];
         }
 
-        // scan line-by-line
-        for (size_t j = 0; j < numRows; j++){
-            for (size_t i = 0; i < numCols; i++){
-                if (data[i][j]) result += data[i][j];
+        // 4. print
+        for (size_t i=0; i<numRows; i++){
+            for (size_t j=0; j<cols; j++){
+                if(rowString[i][j])
+                    result += rowString[i][j];
             }
         }
 
-        // release resource
-        for (size_t i = 0; i < numCols; i++){
-            if (data[i]){
-                free(data[i]);
-                data[i] = NULL;
+        // 5. release resource
+        for (size_t i=0; i< numRows; i++){
+            if(rowString[i]){
+                delete rowString[i];
+                rowString[i] = NULL;
             }
         }
 
-        if (data){
-            free(data);
-            data = NULL;
-        }
-        
         return result;
     }
 };
@@ -98,12 +89,42 @@ public:
 int main(){
     Solution sol;
     string result;
-//    result = sol.convert("sample", 3);
-//    result = sol.convert("vcakvfswgkgszhcscpuhhigcchceedszhujczdaiohapocirreaapicrrfxviruplgtljvtzcvrtxbmqhmrwuvplamjkdfbrlwexsxyusrprubjcncajyqxaeglklobzlhydewzyqprkqcmgfnrdfoflwsvkhylsfjxlhwlxneighwomlfrqwwcupzeyypqfaprinehbjrjqoxezcvsarwvivbgvtybtnuddalgjcpbsofusamtuiocfrldklgebajeaukmzbnrptlhvzcpjsupkhsusyatakxcgjofkezbxmlsmnkvgemqlgmdgnzizwnfidnuhowgajzmwlkepyuchjhnygyxflepotjyhheisfwpqithhqjoztdxbbwioczdwjddshnlnmcduxlbnwrorvntyjdnmdskovpicdvrrxvlvinkegzybmtcywrmbjwpglakqvchvzvshicnqdluqgwqdnceyywglwqetunotigasjqjoddgkzwpoyvoyrumpkqjfdxamgdeptpdysmitixhjtvhmrtcclnjpmjdsmjjhzngrzqnjqwslucxlxbpjoyabkdvyofinuqhvgueyqxjkbjwyklhbmhewmzwbeeqyuxtdrabkxlwausyggghuplscnofrvvsptlsmlwykhvkbpjjxrrrgejkra", 33);
-//    result = sol.convert("PAYPALISHIRING", 3);
-//    result = sol.convert("AB", 1);
+
+//  test set:
+//  output: slapem
+    result = sol.convert("sample", 3);
+    printf("result: %s\n", result.c_str());
+
+//  test set:
+//  output: bwbyvuwpzbbubozknyxuflsgagtjikxjjyjeufutixpjlqvaotuwemehuxsdkpotpnyvzajkddtccoouswtzxodakfv
+    result = sol.convert("bwbyvuwpzbbubozknyxuflsgagtjikxjjyjeufutixpjlqvaotuwemehuxsdkpotpyzjdtcostxdkvfaozwuocdkavn", 78);
+    printf("result: %s\n", result.c_str());
+
+//  test set:
+//  output: PAHNAPLSIIGYIR
+    result = sol.convert("PAYPALISHIRING", 3);
+    printf("result: %s\n", result.c_str());
+
+//  test set:
+//  output: AB
+    result = sol.convert("AB", 1);
+    printf("result: %s\n", result.c_str());
+
+//  test set:
+//  output: ACB
     result = sol.convert("ABC", 2);
     printf("result: %s\n", result.c_str());
+
+//  test set:
+//  output: ""
+    result = sol.convert("", 2);
+    printf("result: %s\n", result.c_str());
+
+//  test set:
+//  output: "ABCDFE"
+    result = sol.convert("ABCDEF", 5);
+    printf("result: %s\n", result.c_str());
+
     return 0;
 }
 
